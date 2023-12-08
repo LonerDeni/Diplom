f46eae25fbb9bf6c265f4b00b395e735c7c154e7
LonerDeni <124445115+LonerDeni@users.noreply.github.com>
2023-12-08 23:20:36 +0300

    fix v2

--- src/main/java/com/example/diplom/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/diplom/advice/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.diplom.advice;
 import com.example.diplom.exception.AuthTokenException;
 import com.example.diplom.exception.CreateUserException;
 import com.example.diplom.exception.FileException;
+import com.example.diplom.exception.ResponseError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +12,22 @@ import org.springframework.web.bind.annotation.*;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> credentialsHandler(FileException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseError> credentialsHandler(FileException e) {
+        //return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(new ResponseError(null,e.getMessage()));
     }
 
     @ExceptionHandler(AuthTokenException.class)
-    public ResponseEntity<String> credentialsHandler(AuthTokenException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ResponseError> credentialsHandler(AuthTokenException e) {
+        //return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ResponseError(null,e.getMessage()));
     }
 
     @ExceptionHandler(CreateUserException.class)
-    public ResponseEntity<String> credentialsHandler(CreateUserException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseError> credentialsHandler(CreateUserException e) {
+        //return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(new ResponseError(null,e.getMessage()));
     }
 }
